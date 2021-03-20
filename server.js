6cdc1511e72a1c0442e223fbe8b014ee193d119c
MarioMarkov <mario11septemvri@gmail.com>
2021-03-20 21:47:33 +0200

    Author routes

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ if(process.env.NODE_ENV!=='production'){
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
-const indexRouter = require('./routes/index')
+const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors')
+
 
 app.set('view engine','ejs');
 app.set('views',__dirname+'/views');
@@ -14,11 +17,12 @@ app.set('layout','layouts/layout');
 
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({limit:'10mb',extended:false}))
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser:true,
-    
+    useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
@@ -29,6 +33,7 @@ db.on('open',()=> console.log('Connected to Mongoose'))
 
 
 app.use('/',indexRouter);
+app.use('/authors',authorRouter);
 
 
 
